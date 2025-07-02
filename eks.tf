fa5dbc6b579232cd26a4133217ca9e3924ed931a
gjame77 <kangjaeku@naver.com>
2025-07-02 14:24:20 +0900

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,1 +1,25 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  enable_irsa               = true
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.small"]
+      name           = "node-group-1"
+    }
+  }
+}
